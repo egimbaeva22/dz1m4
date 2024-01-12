bf9f702b7b608a722dd5f97b0f53623019f29da1
egimbaeva22 <alinaegimbaeva2006@gmail.com>
2024-01-12 23:28:51 +0600

    dz1m4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const user = {
+    name: "Hedy Lamarr",
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 150
+}
+const user1 = {
+    name: "Marilyn Monroe",
+    imageUrl: 'https://untitled-magazine.com/online/wp-content/uploads/2016/06/Marilyn_Monroe_1953_crop.jpg',
+    imageSize: 150
+}
+export default function Profile() {
+    return (
+        <>
+            <div>
+                <h1>{user.name}</h1>
+                <img
+                    className="avatar"
+                    src={user.imageUrl}
+                    alt={"Photo of " + user.name}
+                    style={{ width: user.imageSize, height: user.imageSize }}
+                />
+            </div>
+            <div>
+                <h1>{user1.name}</h1>
+                <img
+                    className="avatar"
+                    src={user1.imageUrl}
+                    alt={"Photo of " + user1.name}
+                    style={{ width: user1.imageSize, height: user1.imageSize }}
+                />
+            </div>
+        </>
+    );
 }
 
-export default App;
+
+
